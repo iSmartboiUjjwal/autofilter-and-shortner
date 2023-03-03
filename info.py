7bc26fd20c7ed935b8bbf319728a00cd343f44f8
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2023-03-03 19:35:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://MovieSearchBot:<password>@moviesearchbotdb.yewhexb.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Professor:Professor@professor.cyhj5tr.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "MxA_Bots")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
