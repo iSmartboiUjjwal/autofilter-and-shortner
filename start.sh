3804e972091a567233e11b0c69069fbbb417d05d
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2023-03-03 18:34:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iSmartboiUjjwal/autofilter-and-shortner /EvaMaria
+  git clone https://github.com/iSmartboiUjjwal/autofilter-and-shortner.git /autofilter-and-shortner
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /autofilter-and-shortner
 fi
-cd /EvaMaria
+cd /autofilter-and-shortner
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
